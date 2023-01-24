ac6b0dcea00e93223f5609b53c6a8e7340595785
Sardeni <nordisnsk@gmail.com>
2023-01-24 20:35:33 +0400

    save commit

---
+++ src/main/java/com/sardeni/Main.java
@@ -0,0 +1,7 @@
+package com.sardeni;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/sardeni/WebSteps.java
@@ -0,0 +1,28 @@
+package com.sardeni;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebSteps {
+
+    @Step("Открываем страницу https://github.com/{page}")
+    public void openPage(String page) {
+        open("https://github.com/" + page);
+    }
+
+    @Step("Вводим в строке поиска issueId {issueId}")
+    public void inputIssueIdInSearchField(String issueId) {
+        $("#js-issues-search").click();
+        $("#js-issues-search").setValue(issueId);
+        $("#js-issues-search").pressEnter();
+    }
+
+    @Step("Проверяем название Issue {issueId}" )
+    public void checkIssueName(String issueId, String issueName) {
+        $("#issue_2133_link").shouldHave(Condition.
+                text(issueName));
+    }
+
+}
